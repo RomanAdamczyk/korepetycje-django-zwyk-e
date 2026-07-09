576d82893176513cb79fb9a41b7049d203c77a2e
RomanAdamczyk <romanadamczyk82@gmail.com>
2026-07-09 20:49:07 +0200

    feat: add shared plot fields to TaskGroup model

--- matematyka/models.py
+++ matematyka/models.py
@@ -28,7 +28,10 @@ class TaskGroup(models.Model):
         name (str): The name of the task group.'''
     
     shared_content = models.CharField(max_length=100, unique=True)
-    
+    pieces = models.JSONField(null=True, blank=True, help_text="Wspólny wykres dla grupy zadań")
+    x_min = models.FloatField(null=True, blank=True)
+    x_max = models.FloatField(null=True, blank=True)    
+
     def __str__(self):
         return self.shared_content
     
